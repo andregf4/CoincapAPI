6f406b1cd98e93ba2e96967346bd0f741b77f053
André Foratto <andre.foratto@eldorado.org.br>
2024-04-12 09:23:06 -0300

    Initial Commit

---
+++ ApiCoinCap/ApiCoinCapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiCoinCapApp.swift
+//  ApiCoinCap
+//
+//  Created by Andre Gerez Foratto on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ApiCoinCapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
